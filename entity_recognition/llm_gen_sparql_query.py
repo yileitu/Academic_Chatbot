3e8e5424c4fb4db217bb9f8fe022e7005ab34c05
yileitu <yileitu@student.ethz.ch>
2024-02-15 22:22:29 +0800

    llama gen sparql queries

--- entity_recognition/llm_gen_sparql_query.py
+++ entity_recognition/llm_gen_sparql_query.py
@@ -4,10 +4,6 @@ from transformers import LlamaForCausalLM, LlamaTokenizer, set_seed
 from final_output.natural_output import set_llama_config
 
 input_text = """
-The following performs a describe query for the paper with id 2040986908.
-DESCRIBE <http://aida.kmi.open.ac.uk/resource/2040986908>
-
-
 The following query returns all papers written by authors from the industrial sector computing and it associated with the topic robotics:
 PREFIX aida-ont:<http://aida.kmi.open.ac.uk/ontology#>
 PREFIX aida:<http://aida.kmi.open.ac.uk/resource/>
@@ -56,7 +52,7 @@ WHERE {
 Please output the SPARQL query that returns the topic distribution of a given affiliation (in this case The Open University)
 """
 
-LLM_PARAM: int = 13
+LLM_PARAM: int = 7
 LLM_NAME: str = f"Llama-2-{LLM_PARAM}b-chat"
 LLM_HF_PATH: str = f"meta-llama/{LLM_NAME}-hf"
 set_seed(42)
